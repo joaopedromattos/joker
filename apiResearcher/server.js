542eb1531a26abe962d546dc1cb3bbc3ffaf0ada
João Pedro <joao_pedro_mattos@hotmail.com>
2019-08-05 15:50:10 -0300

    Fixing database url issue.

--- apiResearcher/server.js
+++ apiResearcher/server.js
@@ -2,43 +2,39 @@ require("dotenv").config();
 var cors = require("cors");
 var express = require("express"),
     helmet = require("helmet"),
-    app = express(), 
+    app = express(),
     port = process.env.PORT || 3000,
     mongoose = require("mongoose"),
-    https = require('https'),
+    https = require("https"),
     Study = require("./model/study"),
     Researcher = require("./model/researcher"),
     Board = require("./model/board"),
     fs = require("fs"),
-    bodyParser = require("body-parser")
+    bodyParser = require("body-parser");
 
 app.use(helmet());
 mongoose.Promise = global.Promise;
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
+const dbUrl = process.env.NODE_ENV === "development" ? "localhost" : "mongo";
 
-const dbUrl = (process.env.NODE_ENV === 'development') ? 'localhost' : 'mongo';
+mongoose.connect("mongodb://" + dbUrl + ":27017/admins", {
+    useNewUrlParser: true
+});
 
-mongoose.connect("mongodb://" + dbUrl + ":27017/admins", { useNewUrlParser: true })
-// mongoose.connect('mongodb+srv://joaopedromattos:PAHgrR3SlzxPy3fT@joker-wrw9o.mongodb.net/test?retryWrites=true', {useNewUrlParser: true}).catch((reason) =>{
-//     console.log("API Server crashed: ", reason);
-// })
-app.use(express.static('./statisticalModule/results'))
-app.use(cors())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(express.static("./statisticalModule/results"));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-var routes = require("./routes/routes")
+var routes = require("./routes/routes");
 
 routes(app);
 
-app.use((req, res) =>{
-    res.status(404).send({url : req.originalUrl + ' not found.'})
-})
+app.use((req, res) => {
+    res.status(404).send({ url: req.originalUrl + " not found." });
+});
 
-app.listen(port, console.log(`API is running on http://${dbUrl}:` + port))
-
-
-  
+app.listen(port, console.log(`API is running on http://${dbUrl}:` + port));
