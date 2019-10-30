ec9c622b5eb23304d30b251ede34d10085aa9109
João Pedro <joao_pedro_mattos@hotmail.com>
2019-10-30 13:24:32 -0300

    Added a landing page based on Material Ui components.
    The next step will be to insert a background image and improve our media query in css.

--- src/app/components/LandingPage/LandingPage.jsx
+++ src/app/components/LandingPage/LandingPage.jsx
@@ -2,135 +2,202 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";import "./LandingPage.scss";
+import { Link } from "react-router-dom";
+import "./LandingPage.scss";
 import classnames from "classnames";
 import slugify from "slugify";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import USP from "./../../../assets/images/usp-logo-png.png";
+import Fapesp from "./../../../assets/images/fapespIcon.png";
 
-class LandingPage extends Component {
-  static propTypes = {
-    // boards: PropTypes.arrayOf(
-    //   PropTypes.shape({
-    //     _id: PropTypes.string.isRequired,
-    //     color: PropTypes.string.isRequired,
-    //     title: PropTypes.string.isRequired
-    //   }).isRequired
-    // ).isRequired,
-    listsById: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired
-  };
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: "auto",
+        backgroundImage: "url('/images/jokerLandingPage.jpg')"
+    },
+    icon: {
+        marginRight: theme.spacing(2)
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+        boxShadow: "2px 2px 14px -4px rgba(0,0,0,0.72)"
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4)
+    },
+    menuButton: {
+        // marginRight: theme.spacing(2)
+    },
+    title: {
+        flexGrow: 1
+    },
+    footer: {
+        padding: theme.spacing(2),
+        marginTop: "auto",
+        backgroundColor: "white",
+        width: "100%",
+        position: "sticky"
+    },
+    fapesp: {
+        paddingTop: "12px",
+        maxWidth: "70px"
+    },
+    usp: {
+        maxWidth: "70px"
+    }
+}));
 
-  enterAsGuest = () => {
-    this.props.dispatch({ type: "ENTER_AS_GUEST" });
-  };
+function LandingPage(props) {
+    const propTypes = {
+        // boards: PropTypes.arrayOf(
+        //   PropTypes.shape({
+        //     _id: PropTypes.string.isRequired,
+        //     color: PropTypes.string.isRequired,
+        //     title: PropTypes.string.isRequired
+        //   }).isRequired
+        // ).isRequired,
+        listsById: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired,
+        dispatch: PropTypes.func.isRequired
+    };
 
-  render = () => {
+    const classes = useStyles();
 
-    const { boards, listsById, history } = this.props;
+    const enterAsGuest = () => {
+        this.props.dispatch({ type: "ENTER_AS_GUEST" });
+    };
 
-
+    const { boards, listsById, history } = props;
 
     return (
-    <div className="landing-page">
-      <Helmet>
-        <title>Joker - An Open Source Card Sorting Application</title>
-      </Helmet>
-      <div className="landing-page-info-wrapper">
-        <div className="landing-page-info">
-          <p className="landing-page-description">
-            Olá,</p>
+        <React.Fragment>
+            <Helmet>
+                <title>Joker - An Open Source Card Sorting Application</title>
+            </Helmet>
+            <CssBaseline />
 
-            <p className="landing-page-description">
-              Esta atividade dura menos de 5 min, as instruções de como completá-la serão exibidas assim que prosseguir para a mesma.
-            </p>
-
-            <p className="landing-page-description">
-              Este formulário é parte de uma pesquisa realizada pelos pesquisadores da Universidade de São Paulo (USP).
-            </p>
-            <ul>
-                <li>Me. André de Lima Salgado (Doutorando)</li>
-                <li>Felipe Silva Dias (Mestrando)</li>
-                <li>Anderson Canale Garcia (Mestrando) </li>
-                <li>Dra. Renata P. M. Fortes (Professora)</li>
-              </ul>
-            <p className="landing-page-description">
-              Nossa pesquisa é sobre o controle (limites no uso) feito por pais e mães sobre o uso da Internet pelos filhos.
-            </p>
-            <p className="landing-page-description">
-              Sua opinião é importante para nos ajudar a descobrir o agrupamento de termos que mais faz sentido para um aplicativo.
-            </p>
-            <p className="landing-page-description">
-              Não se preocupe, não é necessário conhecimento específico para participar, você é quem definirá o agrupamento segundo suas preferências.
-            </p>
-
-            <p className="landing-page-description">
-              Lembramos que todas as informações fornecidas são anônimas, e não visamos identificar sua individualidade em nenhum momento da pesquisa.
-            </p>
-
-            <p className="landing-page-description">
-              Caso aceite participar, clique em “começar”.
-            </p>
-
-            <p className="landing-page-description">
-              Muito obrigado,<br />
-
-              André, Felipe, Anderson e Renata
-            </p>
-
-            <p><i>Agradecimentos  aos  processos  nº  2017/15239
-                -
-                0  e  nº  2015/24525
-                -
-                0,  da  Fundação  de Amparo à Pesquisa do Estado de São Paulo (FAPESP), que financiaram esta pesquisa.</i></p>
-
-            <p><i>
-            As opiniões, hipóteses e conclusões ou recomendações expressas neste material são de responsabilidade do(s) autor(es) e não necessariamente refletem a visão da FAPESP.
-
-              </i></p>
-          <div className="signin-buttons">
-
-            <div className="guest-button-wrapper">
-
-              <Link to="/boardAccess">
-                <button className="signin-button guest-button">
-                  Começar
-                </button>
-              </Link>
-
-              {/* {boards.map(board => (
-                <Link
-                  key={board._id}
-                  to={`/b/${board._id}/${slugify(board.title, {
-                    lower: true
-                  })}`}
-                  onClick={this.enterAsGuest}
-                >
-                  <div className="guest-button-wrapper">
-                    <button className="signin-button guest-button">
-                      Começar
-                  </button>
-                  </div>
-                </Link>
-              ))} */}
-              <br/>
-              <Link to="/researcherAuth">
-                <button className="signin-button guest-button">
-                  Área do pesquisador
-                </button>
-              </Link>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>);
-  }
+            <main className={classes.root}>
+                {/* Hero unit */}
+                <div className={classes.heroContent}>
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="textPrimary"
+                            gutterBottom
+                        >
+                            Joker
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            align="center"
+                            color="textSecondary"
+                            paragraph
+                        >
+                            An Open Source Card Sorting Application.
+                        </Typography>
+                        <div className={classes.heroButtons}>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button
+                                        variant="contained"
+                                        href="/boardAccess"
+                                        color="primary"
+                                    >
+                                        Área do participante
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button
+                                        variant="outlined"
+                                        // href="/researcherAuth"
+                                        color="primary"
+                                    >
+                                        <Link
+                                            to="/researcherAuth"
+                                            style={{
+                                                color: "inherit",
+                                                textDecoration: "inherit"
+                                            }}
+                                        >
+                                            Área do pesquisador
+                                        </Link>
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                        <div className={classes.heroButtons}>
+                            <Typography
+                                variant="caption"
+                                display="block"
+                                align="justify"
+                            >
+                                Agradecimentos aos processos nº 2018/19323-8, nº
+                                2017/15239 - 0 e nº 2015/24525 - 0, da Fundação
+                                de Amparo à Pesquisa do Estado de São Paulo
+                                (FAPESP), que financiaram esta pesquisa e o
+                                desenvolvimento deste projeto. Feito por:
+                                <span style={{ textDecoration: "underline" }}>
+                                    Anderson Canale Garcia, André de Lima
+                                    Salgado, Felipe Silva Dias, João Pedro R.
+                                    Mattos e Renata P. M. Fortes.
+                                </span>
+                            </Typography>
+                        </div>
+                        <br />
+                        <div className={classes.heroButtons}>
+                            <Grid container spacing={2} justify="space-evenly">
+                                <Grid item>
+                                    <img
+                                        src={
+                                            "http://blog.institutocidadejardim.com.br/wp-content/uploads/2018/07/Logotipo-Fapesp-atualizado.png"
+                                        }
+                                        className={classes.fapesp}
+                                    ></img>
+                                </Grid>
+                                <Grid item>
+                                    <img
+                                        src={
+                                            "http://www.scs.usp.br/identidadevisual/wp-content/uploads/2013/08/usp-logo-png.png"
+                                        }
+                                        alt="Logotipo Universidade de São Paulo"
+                                        className={classes.usp}
+                                    />
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+            </main>
+            {/* <footer position="relative" className={classes.footer}>
+                <Container maxWidth="sm">
+                    <Typography variant="body1">
+                        My sticky footer can be found here.
+                    </Typography>
+                </Container>
+            </footer> */}
+        </React.Fragment>
+    );
 }
 
-
 const mapStateToProps = state => ({
-  boards: Object.values(state.boardsById),
-  listsById: state.listsById
+    boards: Object.values(state.boardsById),
+    listsById: state.listsById
 });
 
 export default connect(mapStateToProps)(LandingPage);
+
+/*
+
+*/
